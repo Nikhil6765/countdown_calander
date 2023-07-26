742e230b2095ca33bc4c2747631e14cef4d890d7
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-26 12:13:21 +0530

    added_text

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from datetime import date, datetime
 # creating the canvas
 
 root=Tk()  # creating a tkinter window
+
 c=Canvas(root, width=800, height=800, bd='black')  # canvas of 800 by 800 created
+
 c.pack()  # packs the canvas into tkinter window
+
 c.create_text(100,50,anchor='w',fill='orange',font='Arial 28 bold underline', text='My Countdown Calander')
