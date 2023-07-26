04c8c40b93dd79266901d8ed776c415f6b413772
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-26 12:17:57 +0530

    added_canvas

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from datetime import date, datetime
 
 root=Tk()  # creating a tkinter window
 
-c=Canvas(root, width=800, height=800, bd='black')  # canvas of 800 by 800 created
+c=Canvas(root, width=800, height=800, bg='black')  # canvas of 800 by 800 created
 
 c.pack()  # packs the canvas into tkinter window
 
-c.create_text(100,50,anchor='w',fill='orange',font='Arial 28 bold underline', text='My Countdown Calander')
+c.create_text(100,50,anchor='w',fill='orange',font='Arial 28 bold underline', text='My Countdown Calander')  # this line adds text onto the c canvas
+
+root.mainloop()
