e6d436078319387c7bc7648c3e7528a9758fd08a
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-26 12:09:27 +0530

    canvas

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, Canvas
+from datetime import date, datetime
+
+# creating the canvas
+
+root=Tk()  # creating a tkinter window
+c=Canvas(root, width=800, height=800, bd='black')  # canvas of 800 by 800 created
+ 
